565b394c7e31f73c9f1bd7492ee83595aa430002
Pedro Henrique <pedro.marques@trackcash.com.br>
2021-06-23 17:11:38 -0300

    Corrigindo tag incorreta

--- src/components/modal/actions/styles.js
+++ src/components/modal/actions/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const ModalActionsStyles = styled.aside`
+export const ModalActionsStyles = styled.div`
     margin-top: 40px;
     display: flex;
     justify-content: flex-end;
